b9bf86f5e3570d1e94c3a46bf4a2320835bce532
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-12 14:19:32 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,28 @@
+console.log("Script running...")
+// Task 1: 
+// Select the cshImage div
+let div = document.querySelector("#cshImage")
+
+// Add an event listener such that when the div is clicked, you create a new p tag, set the text to "Our new building!" and append it to the div
+div.innerHTML = <img src = "https://www.lisc.org/media/filer_public_thumbnails/filer_public/cc/53/cc530361-d2df-446d-ab6c-5fd7543fcf06/051623_comp_sci_exterior_march_2023_-_credit_kss_architects.jpg__680x380_q85_crop_subsampling-2_upscale.jpg"></img>
+
+let newTag = document.createElement("p") 
+
+
+// Task 2: 
+// Set up the DOM manipulation flow such that when you click the cat button, the cat image is removed.
+
+
+
+
+// Set up the DOM manipulation flow such that when you click the dog button, the dog image is removed.
+
+
+
+
+// Task 3: Set up the DOM manipulation flow such that when the add button is clicked, you create a new li tag, set the text to the value of input, and append it to the ul element
+
+
+
+
+
